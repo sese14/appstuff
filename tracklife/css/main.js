e2b8b74873ca956f8b4d8a4c54541dbee1dd5563
sebastian <seba6531@easv365.dk>
2019-11-14 09:14:01 +0100

    the base of the operation
    now the real work starts from here

---
+++ tracklife/css/main.js
@@ -0,0 +1,45 @@
+
+var button = document.getElementById('button')
+
+
+button.addEventListener('click', function ()
+{    var x = document.getElementById("foo")
+
+    if (x.style.display === 'none'){
+        x.style.display = 'block';
+    } 
+    else x.style.display = 'none';
+})
+
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+
+
+
+
+
+
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    var uluru = {lat: 54.913185, lng:  9.779275};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
+  
